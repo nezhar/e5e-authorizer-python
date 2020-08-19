6f9ad3421e07f22b813ec13778837133979b76f5
Harald Nezbeda <HNezbeda@anexia-it.com>
2020-08-19 08:11:07 +0200

    Add header token based authorization

--- function.py
+++ function.py
@@ -1,5 +1,6 @@
 def myAuthorizer(event, context):
-
+    access_ok = event['request_headers'].get('x-my-auth-key') == '---my-secret---'
     return {
-        'status': 200,
+        'status': 200 if access_ok else 400,
+        'data': None,
     }
