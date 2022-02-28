8a33619f7c99897cde441bfeed7a6bc8736c22e1
Harald Nezbeda <hnezbeda@anexia-it.com>
2022-02-28 20:12:28 +0100

    Add output function

--- function.py
+++ function.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from re import S
 
 
-def myAuthorizer(event, context):
+def my_authorizer(event, context):
     access_ok = event['request_headers'].get('x-my-auth-key') == '---my-secret---'
     response_200 = {
         'status': 200,
@@ -19,3 +19,10 @@ def myAuthorizer(event, context):
         'data': None,
     }
     return response_200 if access_ok else response_400
+
+
+def my_function(event, context):
+    return {
+        'event': event,
+        'context': context,
+    }
