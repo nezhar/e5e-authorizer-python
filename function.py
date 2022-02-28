3ea131a619dfe566763a93117d879c29956f6c3d
Harald Nezbeda <hnezbeda@anexia-it.com>
2022-02-28 20:02:05 +0100

    Add user data to 200 response

--- function.py
+++ function.py
@@ -1,6 +1,21 @@
+from datetime import datetime
+from re import S
+
+
 def myAuthorizer(event, context):
     access_ok = event['request_headers'].get('x-my-auth-key') == '---my-secret---'
-    return {
-        'status': 200 if access_ok else 400,
+    response_200 = {
+        'status': 200,
+        'data': {
+            'user': {
+                'id': 1,
+                'name': 'My User',
+            },
+            'datetime': datetime.now().isoformat(),
+        },
+    }
+    response_400 = {
+        'status': 400,
         'data': None,
     }
+    return response_200 if access_ok else response_400
